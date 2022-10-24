fc03cc85224815b6053955dfb166cee067918b4e
Claudio Tassis <claudiotassis@gmail.com>
2022-10-24 16:08:50 -0300

    add: working slides by item created

--- pages/stories/[...id].vue
+++ pages/stories/[...id].vue
@@ -1,16 +1,25 @@
+
 <template>
-	<div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-		<div class="max-w-screen-sm sm:text-center sm:mx-auto">
-			<h2 class="mb-4 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none"></h2>
-			<hr class="w-full my-8 border-gray-300" />
-			<p class="text-base text-gray-700 md:text-lg sm:px-4"></p>
-		</div>
-	</div>
+	<div data-slide="slide" class="slide">
+    <div class="slide-items">
+      <img src="../img/img1.jpg" alt="Img 1">
+      <img src="../img/img2.jpg" alt="Img 2">
+      <img src="../img/img3.jpg" alt="Img 3">
+      <img src="../img/img4.jpg" alt="Img 4">
+    </div>
+    <nav class="slide-nav">
+      <div class="slide-thumb"></div>
+      <button class="slide-prev">Anterior</button>
+      <button class="slide-next">Próximo</button>
+    </nav>
+  </div>
+	
 </template>
 
 <script setup>
 const { getItemById } = useDirectusItems();
 const route = useRoute();
+
 
 const post = await getItemById({ collection: "blog", id: route.params.id });
 if (!post) throwError("No article found, 404");
