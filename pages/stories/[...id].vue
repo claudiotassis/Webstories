fa5fc7c29d5842a0e6254bf46743147c651424c9
Claudio Tassis <claudiotassis@gmail.com>
2022-10-25 20:03:21 -0300

    add:images

--- pages/stories/[...id].vue
+++ pages/stories/[...id].vue
@@ -1,17 +1,34 @@
 <template>
   <div data-slide="slide" class="slide">
     <div class="slide-items">
-      <div id="img" style="text-align: center">
+      <div id="img1" style="text-align: center">
         <img
           style="vertical-align: middle"
           src="../img/img1.jpg"
-          alt=""/>
-        <h2 style="vertical-align: middle">{{ post.main_title }}</h2>
+          alt="Img 1"/>
+        <h2 style="vertical-align: middle">{{ post.titles }}</h2>
       </div>
-      <img src="../img/img1.jpg" alt="Img 1" />
-      <img src="../img/img2.jpg" alt="Img 2" />
-      <img src="../img/img3.jpg" alt="Img 3" />
-      <img src="../img/img4.jpg" alt="Img 4" />
+      <div id="img2" style="text-align: center">
+        <img
+          style="vertical-align: middle"
+          src="../img/img2.jpg"
+          alt="Img 2"/>
+        <h2 style="vertical-align: middle">{{ post.titles }}</h2>
+      </div>
+      <div id="img3" style="text-align: center">
+        <img
+          style="vertical-align: middle"
+          src="../img/img3.jpg"
+          alt="Img 3"/>
+        <h2 style="vertical-align: middle">{{ post.titles }}</h2>
+      </div>
+      <div id="img4" style="text-align: center">
+        <img
+          style="vertical-align: middle"
+          src="../img/img4.jpg"
+          alt="Img 4"/>
+        <h2 style="vertical-align: middle">{{ post.titles }}</h2>
+      </div>      
     </div>
     <nav class="slide-nav">
       <div class="slide-thumb"></div>
@@ -38,5 +55,5 @@ const { getItemById } = useDirectusItems();
 const route = useRoute();
 
 const post = await getItemById({ collection: "webstory", id: route.params.id });
-if (!post) throwError("No article found, 404");
+if (!post) throwError("Nenhum webstory encontrado.");
 </script>
